beac25d4f4d5f185706df1fecb224642bd25db12
James Vasile <james@hackervisions.org>
2011-06-25 21:03:12 -0400

    logging

--- config.py
+++ config.py
@@ -25,3 +25,21 @@ opt={'domain':'test.planeteria.org',
 
 opt.update(locals())
 
+import logging
+logger = logging.getLogger('planeteria')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler(os.path.join(BASE_DIR, 'planeteria.log'))
+fh.setLevel(logging.DEBUG)
+
+
+# create console handler with a higher log level
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+# create formatter and add it to the handlers
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+# add the handlers to the logger
+logger.addHandler(fh)
+logger.addHandler(ch)
+
