5cdf123186c784671dee58f2e08f22ab11bd86c9
James Vasile <james@hackervisions.org>
2011-06-28 13:49:14 -0400

    encoding debug

--- admin.py
+++ admin.py
@@ -138,7 +138,7 @@ def update_config(planet):
       else:
          if 'Duffy' in Form.getvalue('name%d' % feed_count, ''):
             a = u"Duffy: %s -=> %s" % (planet.feeds[url]['name'], planet.feeds[url]['name'])
-            b = u"Duffy: %s -=> %s" % (Form.getvalue('name%d' % feed_count, ''), Form.getvalue('name%d' % feed_count, ''))
+            b = u"Duffy: %s" % Form.getvalue('name%d' % feed_count, '').encode('utf-b', 'ignore')
             log.debug(a)
          if not url in planet.feeds:
             planet.feeds[url]={'url':url, 
