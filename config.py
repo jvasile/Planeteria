5dfc87418437112332ad97f573de938afbd27123
dtiburon <aleta.dunne@gmail.com>
2013-01-31 16:10:42 -0800

    tweaks to make it easier to install locally or in a virtual server / sandbox setting

--- config.py
+++ config.py
@@ -7,13 +7,15 @@ CHECK_INTERVAL = 3600 # dload feed once per hour
 base_dir = os.path.dirname(os.path.abspath( __file__ ))
 data_dir = os.path.join(base_dir, "data")
 BASE_HREF_FILE = os.path.join(data_dir, 'base_href')
+
+# TODO: improve error handling for local file setup.
 if os.path.exists(BASE_HREF_FILE):
     with open (BASE_HREF_FILE, 'r') as FILE:
         BASE_HREF = FILE.readline().strip()
     if not BASE_HREF.endswith('/'):
         BASE_HREF += '/'
 else:
-    BASE_HREF = "//"
+    BASE_HREF = "/"
 
 CACHE_FILE = os.path.join(data_dir, "cache.db")
 PLANETS_FILE = os.path.join(data_dir, "planets.db")
@@ -24,6 +26,9 @@ DATA_FORMAT_VERSION = "0.1.0"
 MAX_ENTRIES = 100
 FEED_TIMEOUT = 100
 
+# calculate value of domain here
+# be nice. handle errors
+
 opt={'website_name':"Planeteria",
      'title':"Planeteria",
      'generator':"Planeteria %s" % VERSION,
@@ -32,7 +37,9 @@ opt={'website_name':"Planeteria",
      'template_dir':TEMPLATE_DIR,
      'new_planet_dir':os.path.join(TEMPLATE_DIR, "new_planet"),
      'base_dir':base_dir,
+     'log_dir':os.path.join(base_dir, "log"),
      'base_href':BASE_HREF,
+# Set domain to the precomputed value
      'domain':BASE_HREF.split("//")[1],
      'data_dir':data_dir,
      'output_dir':OUTPUT_DIR,
@@ -49,7 +56,7 @@ opt={'website_name':"Planeteria",
 import logging
 logger = logging.getLogger('planeteria')
 logger.setLevel(logging.DEBUG)
-fh = logging.FileHandler(os.path.join(base_dir, 'planeteria.log'), encoding = "UTF-8")
+fh = logging.FileHandler(os.path.join(opt['log_dir'], 'planeteria.log'), encoding = "UTF-8")
 fh.setLevel(logging.DEBUG)
 fh_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 fh.setFormatter(fh_formatter)
